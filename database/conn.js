8ffa9860f61766b9a9267d730a6d71431072f9f1
Aman Kumar <kraman82351@gmail.com>
2023-06-27 03:18:20 +0530

    comment out the temporary momory connections

--- database/conn.js
+++ database/conn.js
@@ -4,8 +4,8 @@ import { MongoMemoryServer } from "mongodb-memory-server";
 
 async function connect(){
 
-    const mongod = await MongoMemoryServer.create();
-    const getUri = mongod.getUri();
+    // const mongod = await MongoMemoryServer.create();
+    // const getUri = mongod.getUri();
 
     mongoose.set('strictQuery', true)
     // const db = await mongoose.connect(getUri);
